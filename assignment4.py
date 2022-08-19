3dc4dfa04723b046f42bf8d7a1996cedaa539859
Dilbar07 <111411990+Dilbar07@users.noreply.github.com>
2022-08-19 23:02:06 +0530

    Add files via upload

---
+++ assignment4.py
@@ -0,0 +1,34 @@
+#user input Poblems
+#1
+a="Dilbar Hossain"
+print(a)
+#2
+r=int(input("enter a number:"))
+#3
+r,v=int(input("enter a number:")),int(input("enter a number:"))
+print(r+v)
+#4
+radius=int(input("enter radius of circle:"))
+area=(22/7*radius**2)
+print("area of circle is ",area)
+#5
+x=int(input("enter a number:"))
+print(x**2)
+#6
+b,h=int(input("enter base in cm: ")),int(input("enter a height in cm:"))
+print('area of tringle is',1/2*(b*h),'cm')
+#7
+r,v,m=int(input("enter a number:")),int(input("enter a number:")),int(input("enter a number:"))
+print('avg is',(r+v+m)/3) 
+#8
+p,r,t=int(input("enter initial principle balance:")),int(input("enter annual interest:")),int(input("enter time(in year):"))
+A=(p*r*t)/100
+print('your interst is ',A)
+#9
+l,w,h=int(input("enter length:")),int(input("enter width:")),int(input("enter height:"))
+print("volume of cuboid is",l*w*h)
+#10
+l,w=int(input("enter length:")),int(input("enter width:"))
+print("area of rectangle is is",l*w)
+
+
