ef2b023a4275a30071f2e0b422f023b632142a24
Dilbar07 <111411990+Dilbar07@users.noreply.github.com>
2022-08-25 10:27:57 +0530

    Add files via upload

---
+++ asisgnment8.py
@@ -0,0 +1,57 @@
+#1
+x=5
+while(x):
+    print("MySirg")
+    x-=1
+ #2
+x=10
+while(x):
+    print(11-x,end=' ')
+    x-=1
+#3
+x=10
+while(x):
+    print(x,end=' ')
+    x-=1
+#4
+x=1
+while(x<=10*2):
+    if(x%2!=0):
+        print(x,end=" ")
+    x+=1
+#5
+x=20
+while(x):
+    if(x%2!=0):
+        print(x,end=" ")
+    x-=1
+#6
+x=20
+while(x):
+    if(x%2==0):
+        print(22-x,end=" ")
+    x-=1
+#7
+x=20
+while(x):
+    if(x%2==0):
+        print(x,end=" ")
+    x-=1
+#8
+x=10
+while(x):
+    print((11-x)**2,end=' ')
+    x-=1
+x=10
+#9
+while(x):
+    print((11-x)**3,end=' ')
+    x-=1
+#10
+x=10
+while(x):
+    print((11-x)*5,end=' ')
+    x-=1
+
+
+
